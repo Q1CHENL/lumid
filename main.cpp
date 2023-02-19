6ce76fcb8f4b092b8554614dd6ca238c2aa08c95
Qichen Liu 刘启辰 <74327704+Q1CHENL@users.noreply.github.com>
2023-02-19 15:57:55 +0100

    Updated todos

--- main.cpp
+++ main.cpp
@@ -13,3 +13,6 @@ int main(int argc, char *argv[])
 }
 //todo solve bug when multiple at once
 //todo simplify constructor of Sliders
+//todo do disapear when mouse clicked
+//todo custimize keyboard shortcuts
+//todo dynamically initialize sliders using tuple or other data structures
